ddd644c650709a4aa6b0362f690e4609c7975b22
SabbirRshuvo <sabbirhshuvo11@gmail.com>
2023-09-16 16:19:36 +0600

    second commit

--- js/api.js
+++ js/api.js
@@ -1,13 +1,14 @@
 const API_KEY = `d7ba983363877b8887cddd4fcb647090`;
 
-const searchTemparature = ()=>{
+const searchTemparature = () =>{
     const city = document.getElementById('city-name').value;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    console.log(url);
+    // console.log(url);
 
     fetch(url)
     .then(res => res.json())
     .then(data => displayTempareture(data));
+
 }
 
 const displayTempareture = tempareture =>{
